47b2f66bdd51402ea3a75eea0b7c2e51f19a2a20
Fuucodi0X <akmelf20@gmail.com>
2024-05-23 11:41:34 +0300

    created recipecard(version 0.1)

---
+++ components/RecipeCard.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="min-w-[200px] flex flex-col rounded overflow-hidden p-1 m-1 shadow-md relative">
+        <img class="rounded max-h-[200px] object-cover w-full" src="https://th.bing.com/th/id/R.d0836dd0b11b9a482ef6d20bb884e9fa?rik=DEUftt8n%2fR%2b%2bWw&pid=ImgRaw&r=0" alt="reicpe" />
+        
+        <div class="flex items-start gap-1 m-1">
+            <img class="rounded-full m-1 object-cover aspect-square max-w-[50px] overflow-hidden" src="https://th.bing.com/th/id/OIP.VkAMJ7tPlTf22T4vZ2ozjgHaHa?rs=1&pid=ImgDetMain" alt="avator" />
+            
+            <div class="flex flex-col m-1">
+                <h3 class="font-bold">Easy Asian Crispy Honey Chicken</h3>
+                <div class="flex gap-1 font-light">
+                    <span class="basis-full">Like <span class="text-sm">1.2K</span></span>
+                    <span class="basis-full">Rate: <span class="text-sm">4.7</span></span>
+                </div>
+            </div>
+        </div>
+        
+        <div class="absolute bg-[#ff9900] rounded-full p-2 text-[#fff5cc] text-xs uppercase mt-1 ml-1">
+            <span>25 min</span>
+        </div>
+    </div>
+</template>
+
+<script setup>
+    // const {thumbnail,avator,}
+</script>
