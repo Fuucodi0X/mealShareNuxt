4f371cf63e5ce15c3a95811e6c4ea72332886c3c
Fuucodi0X <akmelf20@gmail.com>
2024-05-23 11:42:35 +0300

    playing with the cards(layouts)

---
+++ app.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="grid gap-2 grid-cols-4">
+    <RecipeCard/>
+    <RecipeCard/>
+    <RecipeCard/>
+    <RecipeCard/>
+    <RecipeCard/>
+    <RecipeCard/>
+    <RecipeCard/>
+    <RecipeCard/>
+    <RecipeCard/>
+  </div>
+</template>
