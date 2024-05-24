e3d155f9038c3091734ff9faa605bac3d43af678
Fuucodi0X <akmelf20@gmail.com>
2024-05-24 11:12:58 +0300

    used as entry to pages

--- app.vue
+++ app.vue
@@ -1,14 +1,3 @@
 <template>
-  <!-- <div class="grid gap-2 grid-cols-4">
-    <RecipeCard/>
-    <RecipeCard/>
-    <RecipeCard/>
-    <RecipeCard/>
-    <RecipeCard/>
-    <RecipeCard/>
-    <RecipeCard/>
-    <RecipeCard/>
-    <RecipeCard/>
-  </div> -->
-  <CreateRecipe/>
+  <NuxtPage/>
 </template>
