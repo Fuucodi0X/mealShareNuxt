6ae150733bd9ec81a896b6a287005eb02d0eb018
Fuucodi0X <akmelf20@gmail.com>
2024-05-25 11:10:42 +0300

    modified the pages(removed the dynamic routes)

--- pages/[id]/index.vue
+++
@@ -1,7 +0,0 @@
-<template>
-    <h1>Welcome, {{ id }}</h1>
-</template>
-
-<script setup>
-const { id } = useRoute().params;
-</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,24 @@
 <template>
-    <CreateStep/>
+  <div class="max-h-screeen flex flex-col">
+    <Header/>
+    <div class="flex-grow-1 grid grid-cols-[auto,1fr] overflow-auto">
+      <div class="border">side bar</div>
+      <div class="overflow-x-hidden px-8 pb-4">
+        <div class="border">Catagories</div>
+        <div
+          class="border grid grid-cols-[repeat(auto-fill,minmax(300px,1fr))] gap-4"
+        >
+          <RecipeCard />
+          <RecipeCard />
+          <RecipeCard />
+          <RecipeCard />
+          <RecipeCard />
+          <RecipeCard />
+          <RecipeCard />
+          <RecipeCard />
+          <RecipeCard />
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
