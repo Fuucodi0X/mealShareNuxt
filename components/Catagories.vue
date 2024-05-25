be7596ceaafee5473675224b58be01ca9e45894f
Fuucodi0X <akmelf20@gmail.com>
2024-05-25 21:27:48 +0300

    created catagory component(version 0.1)

---
+++ components/Catagories.vue
@@ -0,0 +1,21 @@
+<script setup>
+const {data: catagories, addCatagory} = useAvalibleCatagories();
+// addCatagory("coffee")
+const selectedCatagory = useSelectedCatagory();
+function setCatagory(catagory) {
+    selectedCatagory.value = catagory
+}
+function isSelected(selectedCatagory,catagory){
+    return selectedCatagory == catagory ? "dark" : "default"
+}
+</script>
+
+<template>
+    <div class="overflow-x-hidden relative">
+        <div class="flex whitespace-nowarp gap-3 transition-transform w-[max-content]">
+            <Button v-for="catagory of catagories" :variant="isSelected(selectedCatagory,catagory)" @click="setCatagory(catagory)" :key="catagory"class = "py-1 px-3 rounded-lg whitespace-nowarp">
+                {{ catagory }}
+            </Button>
+        </div>
+    </div>
+</template>
