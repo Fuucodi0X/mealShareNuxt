1fc0e69eada42c07156d321644953d4b99d345de
Fuucodi0X <akmelf20@gmail.com>
2024-06-09 12:24:34 +0300

    using runtime varibles for auth0

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,13 @@ export default defineNuxtConfig({
   modules: ["@nuxtjs/tailwindcss","nuxt-icon","@nuxtjs/apollo"],
   plugins: ['~/plugins/auth'],
 
+  runtimeConfig: {
+    public: {
+      domain: 'dev-huzqyd0je3vnz75v.us.auth0.com',
+      clientId: 'kZFjHNOPyqIMtSHMzZnsSwnInY8d2xbd',
+    }
+  },
+  
   apollo: {
     clients: {
       default: {
--- plugins/auth.ts
+++ plugins/auth.ts
@@ -1,9 +1,10 @@
 import { createAuth0 } from '@auth0/auth0-vue'
 
 export default defineNuxtPlugin((nuxtApp) => {
+  const config = useRuntimeConfig()
   const auth0 = createAuth0({
-    domain: 'dev-huzqyd0je3vnz75v.us.auth0.com',
-    clientId: 'kZFjHNOPyqIMtSHMzZnsSwnInY8d2xbd',
+    domain: config.public.domain,
+    clientId: config.public.clientId,
     authorizationParams: {
       redirect_uri: 'http://localhost:3000'
     },
