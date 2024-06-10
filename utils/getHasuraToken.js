35192faa79efbfaa87da29244ef20a80ab2715bd
Fuucodi0X <akmelf20@gmail.com>
2024-06-10 11:25:55 +0300

    saved access token to localstorage

--- utils/getHasuraToken.js
+++ utils/getHasuraToken.js
@@ -4,7 +4,9 @@ export async function getHasuraToken () {
     const auth0 = process.client ? useAuth0() : undefined;
     if(process.client){
         if(auth0?.isAuthenticated.value){
-            return await auth0.getAccessTokenSilently()
+            const token = await auth0.getAccessTokenSilently()
+            localStorage.setItem("hasura-token",token)
+            return token
         }
         if(!auth0?.isAuthenticated.value){
             return ""
