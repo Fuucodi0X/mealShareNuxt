d784d12ba220e1c6d086528eb49d49bf80bf4b07
Fuucodi0X <akmelf20@gmail.com>
2024-06-10 11:51:08 +0300

    removed useAuth, using useAuth0

--- composables/useAuth.js
+++
@@ -1,9 +0,0 @@
-export function useAuth() {
-    const token = ref(''); // Initialize with your token
-  
-    const setToken = (newToken) => {
-      token.value = newToken;
-    };
-  
-    return { token, setToken };
-}
