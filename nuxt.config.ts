3af917eb9961b69fa6b18e6217b06242fa098d2e
Fuucodi0X <akmelf20@gmail.com>
2024-06-10 11:27:21 +0300

    added auth header from localStorage

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -17,8 +17,10 @@ export default defineNuxtConfig({
   apollo: {
     clients: {
       default: {
-        httpEndpoint: 'http://localhost:8080/v1/graphql'
-      }
+        httpEndpoint: 'http://localhost:8080/v1/graphql',
+        tokenStorage: 'localStorage',
+        tokenName: 'hasura-token'
+      },
     },
   },
 })
